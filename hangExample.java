8691212b6c20f0fb8f27d38b5d36bc7b5c583220
matstobbe <70009621+matstobbe@users.noreply.github.com>
2020-09-09 07:54:56 +0100

    Create hangExample.java

---
+++ hangExample.java
@@ -0,0 +1,19 @@
+package packMan;
+
+public class hangExample {
+		
+	private static String[] words = {"lucifer","ibuki","banan","cykel"}; // datorn väljer ett av dess ord
+
+	public static String[] getWords() {
+		return words;
+	}
+
+	
+	}
+
+	
+	
+
+	
+	
+	
